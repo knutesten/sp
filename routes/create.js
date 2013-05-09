dadd0ccd28f4314f9fdaa63c6ac9831c958e2134
Knut Esten Melandsø Nekså <knut.neksa@gmail.com>
2013-05-09 22:36:23 +0200

    Creating new protocols now reduces debtLeft on protocls where you owe money.

--- routes/create.js
+++ routes/create.js
@@ -9,9 +9,9 @@ exports.get = function (req, res) {
       // TODO handle error.
     } else {
       // Move the signed in user to the top of the list.
-      var logedIn = req.session.user.username;
+      var loggedIn = req.session.user.username;
       for (var i in users) {
-        if (users[i].username == logedIn) {
+        if (users[i].username == loggedIn) {
           var tmp = users[i];
           users[i] = users[0];
           users[0] = tmp;
@@ -24,54 +24,121 @@ exports.get = function (req, res) {
 };
 
 exports.post = function (req, res) {
-  validateForm(req, function (err, protocols) {
+  validateForm(req, function (err, protocolsToUpdate, newProtocols) {
     if (err) {
-      // TODO: Find some smart way to handle this type of error. Can't really send it with res.render, because you don't have access to the users.
+      // TODO: Find some smart way to handle this type of error. Can't really send it with res.render, because you don't have access to the users. NB: This type of error will never happen before the server side validation is implemented.
     } else {
       // Save all the new protocols to the database in parallel
-      var protocolsLeft = protocols.length;
-      for (var i in protocols) {
-        protocols[i].save(function (err) {
+      var protocolsLeft = newProtocols.length + protocolsToUpdate.length;
+      for (var i in newProtocols) {
+        newProtocols[i].save(function (err) {
           if (err) {
             // TODO: Handle write error.
           }
 
-          //Is true when alle the protocols have been writen to the database.
+          //Is true when all the protocols have been written to the database.
           if (--protocolsLeft == 0){
-            res.redirect('overview');
+            writeDone();
           }
         });
+      }
+
+      for (var i in protocolsToUpdate) {
+        var p = protocolsToUpdate[i];
+        Protocols.update({ _id: p._id }, { debtLeft: p.debtLeft }, function (err, nr) {
+          if (err) {
+            // TODO: Handle error.
+          }
+
+          if (--protocolsLeft == 0) {
+            writeDone();
+          }
+        });
+      }
+
+      function writeDone() {
+        res.redirect('overview');
       }
     }
   });
 };
 
-// TODO: Actual server-side validation. This might not be necessary because the chance of any of the users tampering with the POST or javascript is unlikely.
+// TODO: Actual server-side validation. This might not be necessary because the chance of any of the users tampering with the POST-data or JavaScript validation is unlikely.
 function validateForm(req, callback) {
+  var loggedIn = req.session.user.username;
+  var newProtocols = [];
   var protocols = [];
-  var user = req.session.user.username;
+  var protocolsToUpdate = [];
   var debtors = req.body.debtors;
+
   // Make sure that debtors is a list, or else .length will give the wrong value.
   debtors = debtors instanceof Array?debtors:[debtors];
-  
   var price = parseFloat(req.body.price);
   var debt = price / debtors.length; 
-  console.log(debt);
-  
-  for (var key in debtors) {
-    if (user == debtors[key]) {
-      continue;
-    }
 
-    protocols.push(new Protocols({
-      buyer: user,
-      date: req.body.date,
-      ware: req.body.ware,
-      debtor: debtors[key],
-      originalDebt: debt,
-      debtLeft: debt
-    }));
+  //Remove logged in user from the debtors.
+  if (debtors[0]==loggedIn) { debtors.shift() }
+
+  // Find who the logged in user owes money.
+  var finds = [];
+  // Create the finds and remove yourself from debtors.
+  for (var i = 0; i < debtors.length; i++) {
+    finds.push(Protocols
+        .find({ debtor: loggedIn,
+                buyer: debtors[i]
+        })
+        .sort({ date: 1 }));
   }
 
-  callback(null, protocols);
+  var findsLeft = finds.length;
+  // Execute the finds.
+  for (var i in finds) {
+    order(i, finds[i]);
+  }
+
+  function order(i, find) {
+    find.exec(function(err, result) {
+      if (err) {
+        // TODO: Handle error.
+      }
+
+      console.log(result);
+      protocols[i] = result;
+      if (--findsLeft == 0) {
+        done();
+      }
+    });
+  }
+  
+  function done() { 
+    for (var i in debtors) {
+
+      // Go through the old protocols and change the debtLeft.
+      var debtLeft = debt;
+      for (var j in protocols[i]) {
+        debtLeft = debt;
+        if (debtLeft <= protocols[i][j].debtLeft) {
+          protocols[i][j].debtLeft -= debtLeft;
+          debtLeft = 0;
+          protocolsToUpdate.push(protocols[i][j]);
+          break;
+        } else {
+          debtLeft -= protocols[i][j].debtLeft;
+          protocols[i][j].debtLeft = 0;
+          protocolsToUpdate.push(protocols[i][j]);
+        }
+      }
+
+      newProtocols.push(new Protocols({
+        buyer: loggedIn,
+        date: req.body.date,
+        ware: req.body.ware,
+        debtor: debtors[i],
+        originalDebt: debt,
+        debtLeft: debtLeft
+      }));
+    }
+
+    callback(null, protocolsToUpdate, newProtocols);
+  }
 };
